5dd78877f9f8ff813f401b4292e35fb48a3c3c0b
SuperJakov <140004250+SuperJakov@users.noreply.github.com>
2024-05-04 21:46:40 +0200

    Update Run-script.ps1

--- JS/Script-runner/Run-script.ps1
+++ JS/Script-runner/Run-script.ps1
@@ -1,4 +1,4 @@
-$Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+<#$Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
 $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
 $hwnd = (Get-Process -PID $pid).MainWindowHandle
 if ($hwnd -ne [System.IntPtr]::Zero) {
@@ -10,7 +10,7 @@ else {
   $hwnd = $Proc.MainWindowHandle
   $Type::ShowWindowAsync($hwnd, 0)
 }
-
+#>
 
 $scriptPath = $null;
 if (!$scriptName) {
